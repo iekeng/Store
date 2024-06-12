c0f7b025ce235e95e6c9d194b46c25060da1637f
iekeng <ekeng7.eb@gmail.com>
2024-06-12 21:46:03 +0100

    route apis

---
+++ inventory/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import (SupplierListCreate,
+                    SupplierRetrieveUpdateDestroy,
+                    ItemListCreate,
+                    ItemRetrieveUpdateDestroy)
+
+urlpatterns = [
+  path('item/', ItemListCreate.as_view(), name='item-list-create'),
+  path('item/<int: pk>/', ItemRetrieveUpdateDestroy.as_view(), name='item-detail'),
+  path('supplier/', SupplierListCreate.as_view(), name='supplier-ist-create'),
+  path('supplier/<int: pk>/', SupplierRetrieveUpdateDestroy.as_view(), name='supplier-detail')
+]
