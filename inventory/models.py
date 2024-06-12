b338ff8f1d7bca4abf0a1b839af273183209a219
iekeng <ekeng7.eb@gmail.com>
2024-06-12 13:37:23 +0100

    fix: adjust max_length for Supplier and Item models

--- inventory/models.py
+++ inventory/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-  name = models.CharField(max_length=30)
+  name = models.CharField(max_length=255)
   description = models.TextField()
-  price = models.IntegerField()
-  created_at = models.DateField(auto_now=True)
+  price = models.DecimalField(max_digits=5, decimal_places=2)
+  created_at = models.DateField(auto_now_add=True)
 
   class Meta:
     ordering = ["name", "description", "price", "created_at"]
@@ -14,8 +14,8 @@ class Item(models.Model):
     return self.nameText
   
 class Supplier(models.Mode):
-  name = models.CharField(max_length=30)
-  contact = models.CharField(max_length=30)
+  name = models.CharField(max_length=255)
+  contact_info = models.CharField(max_length=30)
   items = models.ManyToManyField(Item)
 
   class Meta:
