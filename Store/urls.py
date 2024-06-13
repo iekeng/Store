1a3cf6b167763738f7991df75e77665e943dd8d4
iekeng <ekeng7.eb@gmail.com>
2024-06-13 22:22:10 +0100

    fix: api doc url

--- Store/urls.py
+++ Store/urls.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+  openapi.Info(
+    title="My API",
+    default_version='v1',
+    description="Inventory API for a store"
+  ),
+  public=True,
+  permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('inventory.urls')),
+    path('api/', include('inventory.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='api-docs'),
 ]
