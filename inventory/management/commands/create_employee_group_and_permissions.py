a97cefa182d2ea2bc74193e8a79308266e053b74
iekeng <ekeng7.eb@gmail.com>
2024-06-14 11:29:07 +0100

    rename management file

---
+++ inventory/management/commands/create_employee_group_and_permissions.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group, Permission
+
+class Command(BaseCommand):
+  help = "Create employee group with specific permissions"
+
+  def handle(self, *args, **options):
+    staff_group, created = Group.objects.get_or_create(name="Employee")
+
+    if created:
+      self.stdout.write(self.style.SUCCESS("Successfully created Employee group"))
+    else:
+      self.stdout.write(self.style.WARNING("Staff group already exists"))
+
+    
+    permissions = [
+      Permission.objects.get(codename="add_item"),
+      Permission.objects.get(codename="change_item"),
+      Permission.objects.get(codename="delete_item"),
+      Permission.objects.get(codename="view_item"),
+      Permission.objects.get(codename="add_supplier"),
+      Permission.objects.get(codename="change_supplier"),
+      Permission.objects.get(codename="view_supplier")
+    ]
+
+    staff_group.permissions.set(permissions)
+    
